d82d5d51dcb8734c2217353c212b733c41894342
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-03-30 10:18:07 -0300

    Operadores de Atribuição

---
+++ operadores/atribuicao.py
@@ -0,0 +1,8 @@
+resultado = 2
+resultado += resultado
+resultado += 3 # resultado = resultado + 3
+resultado -= 1 # resultado = resultado - 1
+resultado *= 4 # resultado = resultado * 4
+resultado /= 2 # resultado = resultado / 2
+resultado %= 6 # resultado = resultado % 6
+print(resultado)
