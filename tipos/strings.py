108cc6892524074c54e151ab8749b8854591e8e0
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-03-31 19:00:00 -0300

    Strings #1

---
+++ tipos/strings.py
@@ -0,0 +1,34 @@
+nome = 'Samuel-Oldra'
+
+print(nome[0])
+# nome[0] = 'T'
+print('marca d\'água') # escape
+print('marca d\'água' == "marca d'água")
+
+doc = """
+Teste de multiplas linhas
+Segunda linha
+"""
+print(doc)
+doc2 = '''
+Teste de multiplas linhas
+Segunda linha
+'''
+print(doc2)
+print("Texto com multiplas\n\t...linhas")
+
+
+print(nome[0])
+print(nome[5])
+print(nome[-4])
+
+print(nome[:6])
+print(nome[:-6])
+print(nome[7:])
+print(nome[-5:])
+print(nome[2:5])
+print(nome[::])
+print(nome[::2])
+print(nome[1::2])
+print(nome[::-1])
+print(nome[::-2])
