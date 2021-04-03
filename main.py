94db08fd3fe10692240e2ae8e0f60f4c2dcb692c
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-03 00:00:00 -0300

    Tipos Básicos

--- main.py
+++ main.py
@@ -4,4 +4,4 @@
 # import pacote.sub.arquivo
 
 # import tipos.variaveis
-from tipos import variaveis
+from tipos import variaveis, basicos
---
+++ tipos/basicos.py
@@ -0,0 +1,5 @@
+print(type(1))
+print(type(1.1))
+print(type('texto'))
+print(type(False))
+print(type(True))
