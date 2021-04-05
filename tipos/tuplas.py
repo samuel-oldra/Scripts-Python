e6007855177ac8978bc740b4614b7c4c4668944b
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-05 00:00:00 -0300

    Tuplas

---
+++ tipos/tuplas.py
@@ -0,0 +1,18 @@
+nomes = ('Ana', 'Bia', 'Gui', 'Leo', 'Ana')
+
+x = ('Bia')
+print(type(x))
+y = ('Bia',)
+print(type(y))
+
+print('Bia' in nomes)
+
+print(nomes[0])
+print(nomes[1:3])
+print(nomes[1:-1])
+print(nomes[2:])
+print(nomes[:-2])
+
+print(len(nomes))
+print(type(nomes))
+print(nomes)
