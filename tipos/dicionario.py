b85a13dedfcaa331e7f71057648d6f2260469939
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-07 00:00:00 -0300

    Dicionários

---
+++ tipos/dicionario.py
@@ -0,0 +1,12 @@
+
+aluno = {
+    'nome': 'Pedro Henrique',
+    'nota': 9.2,
+    'ativo': True
+}
+
+print(type(aluno))
+print(aluno['nome'])
+print(aluno['nota'])
+print(aluno['ativo'])
+print(len(aluno))
