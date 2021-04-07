1d2addc38c94acf8317a5dc73342b3477890f3a6
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-07 15:20:32 -0300

    WHILE

---
+++ controle/while_1.py
@@ -0,0 +1,21 @@
+
+# x = 10
+#
+# while x:
+#     print(x)
+#     x -= 1
+#
+# print('Fim!')
+
+nota = 0
+qtde = 0
+total = 0
+
+while nota != -1:
+    nota = float(input('Informe a nota ou -1 para sair: '))
+    if nota != -1:
+        qtde += 1
+        total += nota
+
+if qtde: # Evitar divisão por zero
+    print(f'A média da turma é {total / qtde}')
