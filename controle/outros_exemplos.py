11de36ce2f871ebe8c909d9a1c85966f8a57af91
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-07 15:32:07 -0300

    FOR #2 (outros exemplos)

---
+++ controle/outros_exemplos.py
@@ -0,0 +1,22 @@
+
+pessoas = ['Gui', 'Rebeca']
+adjs = ['Sapeca', 'Inteligente']
+
+for p in pessoas:
+    for a in adjs:
+        print(f'{p} é {a}!')
+
+for i in [1, 2, 3]:
+    pass
+
+for i in range(1, 11):
+    if i % 2: # impar
+        continue
+    print(i)
+
+for i in range(1, 11):
+    if i == 5:
+        break;
+    print(i)
+
+print('Fim!')
