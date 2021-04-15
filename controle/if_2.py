25426315ff0d3cbe3c8ac0e3ad9970c99bbaea39
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-15 00:00:00 -0300

    IF #2

---
+++ controle/if_2.py
@@ -0,0 +1,13 @@
+a = 'valor' # True
+a = 0       # False
+a = -1      # True
+a = -0.0001 # True
+a = ''      # False
+a = ' '     # True
+a = []      # False
+a = {}      # False
+
+if a:
+    print('Existe!!!')
+else:
+    print('não existe ou zero ou vazio...')
