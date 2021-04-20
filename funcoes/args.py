477001919ca2ca92d61a9fbfb384a6c6a4c3baef
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-20 00:00:00 -0300

    Argumentos Variáveis

---
+++ funcoes/args.py
@@ -0,0 +1,10 @@
+
+def soma(*nums):
+    total = 0
+    for n in nums:
+        total += n
+    return total
+
+def resultado_final(**kwargs):
+    status = 'aprovado(a)' if kwargs['nota'] >= 7 else 'reprovado(a)'
+    return f'{kwargs["nome"]} foi {status}'
