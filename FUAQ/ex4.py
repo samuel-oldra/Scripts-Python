fd427513094e953243b019379167e373fa97aa32
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-24 00:00:00 -0300

    Exercícios Remunerados

---
+++ FUAQ/ex4.py
@@ -0,0 +1,23 @@
+
+qtd = 0
+inicial = 100
+
+def ehPerfeito(n):
+    soma = 0
+
+    for val in range(1, n):
+        if n % val == 0:
+            soma += val
+
+    if soma == n:
+        return True
+    else:
+        return False
+
+
+while qtd < 4:
+    inicial += 1
+
+    if(ehPerfeito(inicial)):
+        qtd += 1
+        print(inicial, end=' ')
