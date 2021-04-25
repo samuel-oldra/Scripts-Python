93ad76b60d19b24635eeae4240cc198205af65d1
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-25 00:00:00 -0300

    Mais Operadores Ternários

--- operadores/ternario.py
+++ operadores/ternario.py
@@ -4,3 +4,8 @@ grana = 30
 status = 'Em casa' if lockdown or grana >= 100 else 'Uhuuu'
 
 print(f'O status é: {status}')
+
+
+esta_chovendo = True
+print('Estou com as roupas ' + ('secas.', 'molhadas.')[esta_chovendo])
+print('Estou com as roupas ' + ('molhadas.' if esta_chovendo else 'secas.'))
