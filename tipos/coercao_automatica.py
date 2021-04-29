79968fab14841f49768329bae33ece17c7267d8a
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-29 00:00:00 -0300

    Coerção Automática

---
+++ tipos/coercao_automatica.py
@@ -0,0 +1,17 @@
+
+print(10 / 2)
+print(type(10 / 2))
+
+print(10 / 3)
+print(10 // 3)
+print(type(10 // 3))
+
+print(10 // 3.3)
+print(type(10 // 3.3))
+
+print(10 / 2.5)
+print(2 + True)
+print(2 + False)
+
+print(type(1 + 2))
+print(type(1 + 2.5))
