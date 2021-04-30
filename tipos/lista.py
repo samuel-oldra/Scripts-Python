3c3c49c2155b1e0964bee4daf5196be15bd9d9c0
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-04-30 11:55:24 -0300

    Mais Listas

--- tipos/lista.py
+++ tipos/lista.py
@@ -16,3 +16,45 @@ print(nums[6])
 print(nums[-1])
 print(nums[-2])
 print(nums)
+print()
+
+
+
+# Mais sobre listas
+nova_lista = [1, 5, 'Ana', 'Bia']
+
+print(nova_lista)
+
+nova_lista.remove(5)
+
+print(nova_lista)
+
+nova_lista.reverse()
+
+print(nova_lista)
+
+nova_lista.append([3, 4])
+
+print(nova_lista)
+print(nova_lista.index('Ana'))
+print(nova_lista[0])
+print(1 in nova_lista)
+print('Bia' in nova_lista)
+print('Pedro' not in nova_lista)
+print(nova_lista[-1])
+print()
+
+
+
+lista = ['Ana', 'Lia', 'Rui', 'Paulo', 'Dani']
+print(lista[1:3])
+print(lista[1:-1])
+print(lista[1:])
+print(lista[:-1])
+print(lista[:])
+print(lista[::2])
+print(lista[::-1])
+del lista[2]
+print(lista)
+del lista[1:]
+print(lista)
