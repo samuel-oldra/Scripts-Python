cfc99a8f633384b7161dc35b45b420be7a469558
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-05-04 00:00:00 -0300

    Mais Tuplas

--- tipos/tuplas.py
+++ tipos/tuplas.py
@@ -16,3 +16,29 @@ print(nomes[:-2])
 print(len(nomes))
 print(type(nomes))
 print(nomes)
+print()
+
+
+
+tupla = tuple()
+print(type(tupla))
+tupla = ()
+print(type(tupla))
+# print(dir(tuple))
+# help(tuple)
+
+tupla = ('um')
+print(type(tupla))
+tupla = ('um',)
+print(type(tupla))
+print(tupla[0])
+# tupla[0] = 'novo'
+
+cores = ('verde', 'amarelo', 'azul', 'branco')
+print(cores[0])
+print(cores[-1])
+print(cores[1:])
+
+print(cores.index('amarelo'))
+print(cores.count('azul'))
+print(len(cores))
