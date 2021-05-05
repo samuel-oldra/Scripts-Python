ce513f2af63f02da28d3e685d8aa7bd339852ea9
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-05-05 10:45:54 -0300

    Mais Dicionários

--- tipos/dicionario.py
+++ tipos/dicionario.py
@@ -10,3 +10,49 @@ print(aluno['nome'])
 print(aluno['nota'])
 print(aluno['ativo'])
 print(len(aluno))
+print()
+
+
+
+pessoa = {
+    'nome': 'Prof(a). Ana',
+    'idade': 38,
+    'cursos': ['Inglês', 'Português']
+}
+print(type(pessoa))
+print(dir(dict))
+print(len(pessoa))
+print(pessoa)
+print(pessoa['nome'])
+print(pessoa['idade'])
+print(pessoa['cursos'])
+print(pessoa['cursos'][1])
+# print(pessoa['tags'])
+
+print(pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+
+print(pessoa.get('idade'))
+print(pessoa.get('tags'))
+print(pessoa.get('tags', []))
+print()
+
+
+
+pessoa = {
+    'nome': 'Prof. Alberto',
+    'idade': 43,
+    'cursos': ['React', 'Python']
+}
+pessoa['idade'] = 44
+pessoa['cursos'].append('Angular')
+print(pessoa)
+print(pessoa.pop('idade'))
+print(pessoa)
+pessoa.update({'idade': 40, 'sexo': 'M'})
+print(pessoa)
+del pessoa['cursos']
+print(pessoa)
+pessoa.clear()
+print(pessoa)
